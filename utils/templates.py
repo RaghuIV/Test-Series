6bdc8c48bc4ed4652e71bde6f174ab8aa7ede41a
Gajendra Nishad <75714258+gajjug004@users.noreply.github.com>
2025-07-13 11:30:27 +0530

    completed core module

--- utils/templates.py
+++ utils/templates.py
@@ -1,4 +1,13 @@
 def reset_password_email_template(first_name, last_name, reset_link):
+    """
+    Generate HTML email content for password reset.
+
+    :param first_name: First name of the user
+    :param last_name: Last name of the user
+    :param reset_link: Password reset URL to include in the email
+    :return: HTML string for the password reset email content
+    :rtype: str
+    """
     return f"""
     <p>Hello, {first_name} {last_name}</p>
     <p>Click the link below to reset your password:</p>
